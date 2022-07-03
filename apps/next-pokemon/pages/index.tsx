1856362b35ba65212ecfed691c6a076ed71c8dc8
girordo <tsgiroldo@gmail.com>
2022-07-03 18:55:12 -0300

    :adhesive_bandage: | Simple fix in SSR

--- apps/next-pokemon/pages/index.tsx
+++ apps/next-pokemon/pages/index.tsx
@@ -2,13 +2,33 @@ import React, { useCallback, useEffect, useState } from 'react';
 import type { Pokemon } from '@nx-pokemon-monorepo/shared-types';
 import styles from './index.module.scss';
 
-export function Index() {
+export async function getServerSideProps(ctx) {
+  let pokemon = [];
+  if (ctx.query.q) {
+    const res = await fetch(`http://localhost:3333/search?q=${ctx.query.q}`);
+    pokemon = await res.json();
+  }
+  return {
+    props: {
+      q: ctx.query.q ?? '',
+      pokemon,
+    },
+  };
+}
+
+export function Index({
+  q,
+  pokemon: initialPokemon,
+}: {
+  q: string;
+  pokemon: Pokemon[];
+}) {
   const [search, setSearch] = useState('');
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [pokemon, setPokemon] = useState<Pokemon[]>(initialPokemon);
 
   useEffect(() => {
-    fetch(`http://localhost:3333/search?q=${search}`)
-      .then((res) => res.json())
+    fetch(`http://localhost:3333/search?q=${escape(search)}`)
+      .then((resp) => resp.json())
       .then((data) => setPokemon(data));
   }, [search]);
 
