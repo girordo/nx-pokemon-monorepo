5a06b84d20b700aef985ecff507beb8a1cdd621f
girordo <tsgiroldo@gmail.com>
2022-07-03 18:35:20 -0300

    :sparkles: | Introducing pokemon list

--- apps/next-pokemon/pages/index.tsx
+++ apps/next-pokemon/pages/index.tsx
@@ -1,16 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import type { Pokemon } from '@nx-pokemon-monorepo/shared-types';
 import styles from './index.module.scss';
 
 export function Index() {
   const [search, setSearch] = useState('');
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
 
   useEffect(() => {
     fetch(`http://localhost:3333/search?q=${search}`)
       .then((res) => res.json())
-      .then((data) => setSearch(data));
+      .then((data) => setPokemon(data));
   }, [search]);
 
-  return <div className={styles.page}></div>;
+  const onSetSearch = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value),
+    []
+  );
+
+  return (
+    <div className={styles.page}>
+      <input value={search} onChange={onSetSearch} />
+      <ul>
+        {pokemon.map(({ name: { english }, id }) => (
+          <li key={id}>{english}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Index;
